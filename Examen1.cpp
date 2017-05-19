89ec767231959084ab5fe410a482f45ca93ace39
David <david_elvir@unitec.edu>
2017-05-19 15:18:34 -0600

    agregar y borrar una obra

--- Examen1.cpp
+++ Examen1.cpp
@@ -38,18 +38,81 @@ int main(){
         string nombre;
         string autor;
         string fecha;
-        cout<<"Ingrese los datos generales:"<<endl;
+        cout<<"Ingrese los datos generales: "<<endl;
         getline(cin,nombre);
-        cout<<"Nombre de la obra:";
+        cout<<"Nombre de la obra: ";
         getline(cin,nombre);
-        cout<<"Autor de la obra:";
+        cout<<"Autor de la obra: ";
         getline(cin,autor);
-        cout<<"Fecha en que se ingresa:";
+        cout<<"Fecha en que se ingresa: ";
         getline(cin,fecha);
-        
-             
-        break;
-             }
+        cout<<endl;
+
+        cout<<"-----Elija el tipo de obra que desea agregar-----"<<endl;
+        cout<<"1. Literatura"<<endl;
+        cout<<"2. Escultura"<<endl;
+        cout<<"3. Pintura"<<endl;
+        cout<<"4. Diseño arquitectonico"<<endl;
+        int op2;
+        cin>>op2;
+        switch(op2){
+          case 1:{
+            string genero;
+            string epoca;
+            cout<<"Ingrese el genero: ";
+            cin>>genero;
+            cout<<"Ingrese la epoca: ";
+            cin>>epoca;
+            Literatura lit(genero,epoca,id,nombre,autor,fecha);
+            obras.push_back(lit);
+            //cout<<obras[0].getNombre();
+            break;
+            }
+          case 2:{
+            double peso;
+            string material;
+            cout<<"Ingrese el peso: ";
+            cin>>peso;
+            cout<<"Ingrese el material: ";
+            cin>>material;
+            Escultura esc(peso,material,id,nombre,autor,fecha);
+            obras.push_back(esc);
+            break;
+            }
+          case 3:{
+            string material;
+            string tecnica;
+            cout<<"Ingrese el material del lienzo: ";    
+            cin>>material;
+            cout<<"Ingrese la tecnica usada: ";
+            cin>>tecnica;
+            Pintura pin(material,tecnica,id,nombre,autor,fecha);
+            obras.push_back(pin);
+            break;
+          }
+          case 4:{
+            string terreno;
+            cout<<"Ingrese el terreno: ";
+            cin>>terreno;
+            Diseno dis(terreno,id,nombre,autor,fecha);
+            obras.push_back(dis);
+            break;
+            }
+        }
+          break;
+        }
+        case 2:{
+          int size = obras.size();
+          cout<<"Elija la obra que desea borrar."<<endl;
+          for(int i = 0; i < size; i++){
+            cout<<i<<": "<<obras[i].getNombre()<<endl;
+          }
+          int borrar;
+          cin>>borrar;
+          obras.erase(obras.begin()+borrar);
+          break;
+        }
+      
       /*case 6:
         vivo = false;
         cout << "Ha salido!"<<endl;
